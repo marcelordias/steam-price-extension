bfefb98adcd88be6241f528eae4892b0c1222379
Celinho-o <mrclo_1999@hotmail.com>
2025-05-21 19:50:20 +0100

    server bug fixs

--- prices_server/src/services/allkeyshop.service.ts
+++
@@ -1,5 +0,0 @@
-import { AllkeyshopService } from "allkeyshop-api";
-
-export const createAllkeyshopService = (currency: string, platform: string) => {
-  return new AllkeyshopService({ currency, platform });
-};
